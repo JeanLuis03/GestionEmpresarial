713c574242ccfcfc3a54b20deb5bc0bbd2723328
Jean Luis De Los Santos <jeanluisdelossantosgarabito@gmail.com>
2026-06-08 00:37:42 -0400

    Add project files.

---
+++ GestionEmpresarial/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using GestionEmpresarial.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GestionEmpresarial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
