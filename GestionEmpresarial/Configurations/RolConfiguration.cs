77b4ee675bcd97b8253c677f4c927f51864a754b
Jean Luis De Los Santos <jeanluisdelossantosgarabito@gmail.com>
2026-06-09 01:12:00 -0400

    feat(db): configurar entidades y relaciones en EF Core
    Se agregaron configuraciones para las entidades `Usuario`, `Rol`,
    `Permiso` y `PermisosRol` utilizando Fluent API. Estas incluyen
    mapeos de tablas, claves primarias, restricciones de propiedades
    (longitud máxima, unicidad, valores predeterminados) y relaciones
    entre entidades.
    
    Además, se actualizó `ApplicationDbContext` para incluir `DbSet`
    de las entidades mencionadas y aplicar las configuraciones
    automáticamente con `ApplyConfigurationsFromAssembly`. Esto mejora
    la estructura, mantenibilidad y claridad de la configuración de
    Entity Framework Core.

---
+++ GestionEmpresarial/Configurations/RolConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using GestionEmpresarial.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GestionEmpresarial.Configurations
+{
+    public class RolConfiguration : IEntityTypeConfiguration<Rol>
+    {
+        public void Configure(EntityTypeBuilder<Rol> builder)
+        {
+            builder.ToTable("Roles");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nombre)
+                   .HasMaxLength(100)
+                   .IsRequired();
+
+            builder.Property(x => x.Descripcion)
+                   .HasMaxLength(250);
+
+            builder.Property(x => x.Activo)
+                   .IsRequired();
+        }
+    }
+}
---
+++ GestionEmpresarial/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using GestionEmpresarial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionEmpresarial.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        { }
+
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        public DbSet<Rol> Roles => Set<Rol>();
+        public DbSet<Permiso> Permisos => Set<Permiso>();
+        public DbSet<PermisosRol> PermisosRoles => Set<PermisosRol>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
