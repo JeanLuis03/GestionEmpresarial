6b8cd68ef01d8550560fc731fd7a505b857f90e1
Jean Luis De Los Santos <jeanluisdelossantosgarabito@gmail.com>
2026-06-09 02:04:41 -0400

    feat(auth): agregar servicio para manejo de contraseñas
    Se añadió la interfaz `IPasswordService` en el namespace
    `GestionEmpresarial.Interfaces`, la cual define los métodos
    `HashPassword` y `VerifyPassword` para el manejo de contraseñas.
    
    Se implementó la clase `PasswordService` en el namespace
    `GestionEmpresarial.Services`, que utiliza `PasswordHasher<Usuario>`
    de `Microsoft.AspNetCore.Identity` para realizar el hashing y la
    verificación de contraseñas.
    
    Se registró `IPasswordService` y su implementación en el contenedor
    de inyección de dependencias en `Program.cs` mediante
    `builder.Services.AddScoped<IPasswordService, PasswordService>()`.

--- GestionEmpresarial/Program.cs
+++ GestionEmpresarial/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IPasswordService, PasswordService>();
 
 var app = builder.Build();
 
---
+++ GestionEmpresarial/Services/PasswordService.cs
@@ -0,0 +1,33 @@
+﻿using GestionEmpresarial.Interfaces;
+using GestionEmpresarial.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace GestionEmpresarial.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        private readonly PasswordHasher<Usuario> _passwordHasher;
+
+        public PasswordService()
+        {
+            _passwordHasher = new PasswordHasher<Usuario>();
+        }
+
+        public string HashPassword(string password)
+        {
+            return _passwordHasher.HashPassword(new Usuario(),password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            var result = _passwordHasher.VerifyHashedPassword(
+            new Usuario(),
+            passwordHash,
+            password);
+
+            return result == PasswordVerificationResult.Success;
+        }
+
+
+    }
+}
