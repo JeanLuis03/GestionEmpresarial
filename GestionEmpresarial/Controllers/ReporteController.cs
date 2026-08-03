fb9bf4137b8f8be6a52171f639c90d9415a98555
Jean Luis De Los Santos <jeanluisdelossantosgarabito@gmail.com>
2026-08-03 18:31:26 -0400

    refactor: mejorar la integración de la reportería.

--- GestionEmpresarial/Controllers/ReporteController.cs
+++ GestionEmpresarial/Controllers/ReporteController.cs
@@ -1,12 +1,21 @@
 using GestionEmpresarial.Attributes;
 using GestionEmpresarial.Helpers.Constants;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace GestionEmpresarial.Controllers
 {
     [PermissionAuthorize(PermisosSistema.Consultar)]
     public class ReporteController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public ReporteController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [PermissionAuthorize(PermisosSistema.Consultar)]
         public IActionResult Index()
         {
             return View();
@@ -14,7 +23,9 @@ namespace GestionEmpresarial.Controllers
 
         public IActionResult Productos()
         {
-            return View();
+            var servidor = _configuration["Reportes:Servidor"];
+            var url = $"{servidor}?/Productos&rs:Command=Render";
+            return Redirect(url);
         }
     }
 }
